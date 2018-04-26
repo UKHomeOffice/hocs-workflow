3638fdcf6ad234a2d3a2412dd833abec9b9934e4
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-04-26 13:54:32 +0100

    Initial commit

---
+++ src/main/java/uk/gov/digital/ho/hocs/casework/HocsCaseApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.digital.ho.hocs.casework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocsCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocsCaseApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/gov/digital/ho/hocs/casework/HocsCaseApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.gov.digital.ho.hocs.casework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HocsCaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
