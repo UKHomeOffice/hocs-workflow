5d03d22a3aa2985136864e111b5d910db1fa4757
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-19 13:59:16 +0100

    Implement basic auth on rest calls to casework

--- src/main/java/uk/gov/digital/ho/hocs/workflow/caseworkClient/CaseworkClient.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/caseworkClient/CaseworkClient.java
@@ -3,6 +3,10 @@ package uk.gov.digital.ho.hocs.workflow.caseworkClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +15,13 @@ import uk.gov.digital.ho.hocs.workflow.model.CaseType;
 import uk.gov.digital.ho.hocs.workflow.model.DocumentType;
 import uk.gov.digital.ho.hocs.workflow.model.StageType;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 @Component
@@ -21,18 +29,20 @@ public class CaseworkClient {
 
     private final String CASE_SERVICE;
     private final RestTemplate restTemplate;
+    private final String CASE_SERVICE_AUTH;
 
     @Autowired
-    public CaseworkClient(RestTemplate restTemp, @Value("${hocs.case-service}") String caseService) {
+    public CaseworkClient(RestTemplate restTemp, @Value("${hocs.case-service}") String caseService, @Value("${hocs.case-service.auth}") String caseworkBasicAuth) {
         CASE_SERVICE = caseService;
         restTemplate = restTemp;
+        CASE_SERVICE_AUTH = caseworkBasicAuth;
     }
 
     public CwCreateCaseResponse createCase(CaseType caseType) throws EntityCreationException {
         log.info("Creating a case: {}", caseType);
         if(caseType != null) {
             CreateCaseRequest request = new CreateCaseRequest(caseType);
-            ResponseEntity<CwCreateCaseResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case", request, CwCreateCaseResponse.class);
+            ResponseEntity<CwCreateCaseResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case",  new HttpEntity<>(request, createAuthHeaders()), CwCreateCaseResponse.class);
             if(response.getStatusCodeValue() == 200) {
                 log.debug("Successfully created case: {}", caseType);
                 return response.getBody();
@@ -48,7 +58,7 @@ public class CaseworkClient {
         log.info("Creating a stage for case: '{}' -  type: '{}'", caseUUID, stageType);
         if(caseUUID != null && stageType != null) {
             CreateStageRequest request = new CreateStageRequest(stageType, new HashMap<>());
-            ResponseEntity<CwCreateStageResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/stage" , request, CwCreateStageResponse.class);
+            ResponseEntity<CwCreateStageResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/stage" ,  new HttpEntity<>(request, createAuthHeaders()), CwCreateStageResponse.class);
             if(response.getStatusCodeValue() == 200) {
                 log.debug("Successfully created stage: '{}' - '{}' - '{}'", caseUUID, response.getBody().getUuid(), stageType);
                 return response.getBody();
@@ -64,7 +74,7 @@ public class CaseworkClient {
         log.info("Creating screen '{}' for stage: '{}'", screenName, stageUUID);
         if(caseUUID != null && stageUUID != null && screenName != null && data != null) {
             CreateScreenRequest request = new CreateScreenRequest(screenName,data);
-            ResponseEntity<Void> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/stage/" + stageUUID + "/screen" , request, Void.class);
+            ResponseEntity<Void> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/stage/" + stageUUID + "/screen" ,  new HttpEntity<>(request, createAuthHeaders()), Void.class);
 
             if(response.getStatusCodeValue() == 200) {
                 log.debug("Created screen: '{}' for case '{}'",screenName, stageUUID);
@@ -79,7 +89,7 @@ public class CaseworkClient {
     public void updateStage(UUID caseUUID, UUID stageUUID) throws EntityCreationException {
         log.info("Updating stage '{}' for case: '{}'", stageUUID, caseUUID);
         if(caseUUID != null && stageUUID != null) {
-            ResponseEntity<Void> response = restTemplate.getForEntity(CASE_SERVICE + "/case/" + caseUUID + "/stage/" + stageUUID , Void.class);
+            ResponseEntity<Void> response = restTemplate.exchange(CASE_SERVICE + "/case/" + caseUUID + "/stage/" + stageUUID, HttpMethod.GET, new HttpEntity<>(createAuthHeaders()), Void.class);
 
             if(response.getStatusCodeValue() == 200) {
                 log.debug("Successfully updated stage: '{}' for case '{}'",stageUUID, caseUUID);
@@ -95,7 +105,7 @@ public class CaseworkClient {
         log.info("Creating document for case '{}'", caseUUID);
         if(caseUUID != null && name != null && type != null) {
             CreateDocumentRequest request = new CreateDocumentRequest(name, type);
-            ResponseEntity<CwCreateDocumentResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/document", request, CwCreateDocumentResponse.class);
+            ResponseEntity<CwCreateDocumentResponse> response = restTemplate.postForEntity(CASE_SERVICE + "/case/" + caseUUID + "/document",  new HttpEntity<>(request, createAuthHeaders()), CwCreateDocumentResponse.class);
             if(response.getStatusCodeValue() == 200) {
                 log.debug("Successfully added document ('{}') to case: '{}'", response.getBody().getUuid(), caseUUID);
                 return response.getBody();
@@ -106,4 +116,13 @@ public class CaseworkClient {
             throw new EntityCreationException("Could not add document; caseUUID or caseType is null!");
         }
     }
+
+    private  HttpHeaders createAuthHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add(AUTHORIZATION, caseworkBasicAuth());
+        return headers;
+    }
+
+    public String caseworkBasicAuth() { return String.format("Basic %s", Base64.getEncoder().encodeToString(CASE_SERVICE_AUTH.getBytes(Charset.forName("UTF-8")))); }
 }
