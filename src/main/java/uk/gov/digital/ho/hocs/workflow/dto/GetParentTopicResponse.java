4ffafcb41b82a57d475ce952eecff7ac62f5d83e
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-09-14 14:27:00 +0100

    Fix ParentTopicReponse dto

--- src/main/java/uk/gov/digital/ho/hocs/workflow/dto/GetParentTopicResponse.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/dto/GetParentTopicResponse.java
@@ -3,14 +3,16 @@ package uk.gov.digital.ho.hocs.workflow.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor()
+@NoArgsConstructor
 @Getter
 public class GetParentTopicResponse {
 
-    @JsonProperty("topics")
-    private final List<Topic> topics;
+    @JsonProperty("parentTopics")
+    private List<Topic> parentTopics;
 
 }
--- src/main/java/uk/gov/digital/ho/hocs/workflow/dto/Topic.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/dto/Topic.java
@@ -3,17 +3,19 @@ package uk.gov.digital.ho.hocs.workflow.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor()
+@NoArgsConstructor
 @Getter
 public class Topic {
 
     @JsonProperty("value")
-    private final UUID value;
+    private UUID value;
 
     @JsonProperty("label")
-    private final String label;
+    private String label;
 
 }
