79af643c4eb28116db2a8b9d4c01465da30ad3b3
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-11-14 11:46:04 +0000

    rename caseType caseDataType

--- src/main/java/uk/gov/digital/ho/hocs/workflow/caseworkClient/dto/GetCaseworkCaseTypeResponse.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/caseworkClient/dto/GetCaseworkCaseTypeResponse.java
@@ -3,14 +3,14 @@ package uk.gov.digital.ho.hocs.workflow.caseworkClient.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import uk.gov.digital.ho.hocs.workflow.model.CaseType;
+import uk.gov.digital.ho.hocs.workflow.model.CaseDataType;
 
 @NoArgsConstructor
 @Getter
 public class GetCaseworkCaseTypeResponse {
 
     @JsonProperty("type")
-    private CaseType type;
+    private CaseDataType type;
 
 
 }
