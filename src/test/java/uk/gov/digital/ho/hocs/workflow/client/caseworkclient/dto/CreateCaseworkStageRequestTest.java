f7e0a28644708eff94c7ca79cb6d6efcc1ac654a
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-12-12 12:54:32 +0000

    Move notify out of Workflow into casework. Pass the email types from workflow to casework on each stage creation rather than on specific email tasks.

--- src/test/java/uk/gov/digital/ho/hocs/workflow/client/caseworkclient/dto/CreateCaseworkStageRequestTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/client/caseworkclient/dto/CreateCaseworkStageRequestTest.java
@@ -15,26 +15,27 @@ public class CreateCaseworkStageRequestTest {
 
         StageType stageType = StageType.DCU_MIN_MARKUP;
         UUID teamUUID = UUID.randomUUID();
-        UUID userUUID = UUID.randomUUID();
         LocalDate deadline = LocalDate.now();
+        String allocationType = "anyType";
 
-        CreateCaseworkStageRequest createStageRequest = new CreateCaseworkStageRequest(stageType, teamUUID, deadline);
+        CreateCaseworkStageRequest createStageRequest = new CreateCaseworkStageRequest(stageType, teamUUID, deadline, allocationType);
 
         assertThat(createStageRequest.getType()).isEqualTo(stageType);
         assertThat(createStageRequest.getTeamUUID()).isEqualTo(teamUUID);
         assertThat(createStageRequest.getDeadline()).isEqualTo(deadline);
+        assertThat(createStageRequest.getAllocationType()).isEqualTo(allocationType);
 
     }
 
     @Test
     public void getCreateStageRequestNull() {
 
-        CreateCaseworkStageRequest createStageRequest = new CreateCaseworkStageRequest(null, null,  null);
+        CreateCaseworkStageRequest createStageRequest = new CreateCaseworkStageRequest(null, null,  null, null);
 
         assertThat(createStageRequest.getType()).isNull();
         assertThat(createStageRequest.getTeamUUID()).isNull();
         assertThat(createStageRequest.getDeadline()).isNull();
-
+        assertThat(createStageRequest.getAllocationType()).isNull();
     }
 
 }
