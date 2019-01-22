d430f1fd20d3761e7889b6c91a50d83c58028cc6
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2019-01-22 09:09:02 +0000

    initial migration

--- src/main/java/uk/gov/digital/ho/hocs/workflow/migration/MigrationWorkflowResource.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/migration/MigrationWorkflowResource.java
@@ -4,8 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uk.gov.digital.ho.hocs.workflow.api.WorkflowService;
 import uk.gov.digital.ho.hocs.workflow.api.dto.*;
+import uk.gov.digital.ho.hocs.workflow.domain.model.CaseDataType;
+import uk.gov.digital.ho.hocs.workflow.security.Allocated;
+import uk.gov.digital.ho.hocs.workflow.security.AllocationLevel;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -20,6 +26,18 @@ class MigrationWorkflowResource {
     public MigrationWorkflowResource(MigrationWorkflowService workflowService) {
         this.workflowService = workflowService;
     }
+
+//    @PostMapping(value = "migration/case", consumes = APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity<CreateCaseResponse> createCase(@RequestBody MigrationCreateCaseRequest request) {
+//        CreateCaseResponse response = workflowService.createCase(request.getType(), request.getCaseReference(), request.getDateReceived(), request.getCaseDeadline(), request.getData(), request.getDocuments(), request.getCorrespondent(), request.getTopic());
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    @PostMapping(value = "migration/case/{caseUUID}/document", produces = APPLICATION_JSON_UTF8_VALUE)
+//    public ResponseEntity createDocument(@PathVariable UUID caseUUID,@RequestBody CreateDocumentRequest request) {
+//        workflowService.createDocument(caseUUID, request.getDocuments());
+//        return ResponseEntity.ok().build();
+//    }
 
     @PostMapping(value = "migration/case", consumes = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<MigrationCreateCaseResponse> createCase(@RequestBody MigrationCreateCaseRequest request) {
