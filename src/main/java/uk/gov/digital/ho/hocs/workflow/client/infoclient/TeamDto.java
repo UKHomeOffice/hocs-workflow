e1bf806b9baa7f062755a8c730d04731aaa6a901
Stuart <stuart.whitlam@digital.homeoffice.gov.uk>
2019-01-24 22:08:16 +0000

    add no args constructor for team dto

--- src/main/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/TeamDto.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/TeamDto.java
@@ -3,10 +3,12 @@ package uk.gov.digital.ho.hocs.workflow.client.infoclient;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class TeamDto {
 
