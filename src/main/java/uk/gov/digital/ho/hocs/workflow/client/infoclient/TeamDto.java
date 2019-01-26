a07d1c8fac1a4b5431d3fc9191d16b6a27d52771
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-01-26 10:11:49 +0000

    fix dten dispatch by adding another stage.

--- src/main/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/TeamDto.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/TeamDto.java
@@ -3,12 +3,15 @@ package uk.gov.digital.ho.hocs.workflow.client.infoclient;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
-public class TeamDto {
+public class TeamDto implements Serializable {
 
     @JsonProperty("displayName")
     private String displayName;
