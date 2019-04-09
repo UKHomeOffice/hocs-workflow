051097b7e0a0b0025c23f2da0e8a70c4c30ecd5c
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-09 10:48:49 +0100

    Update CaseDataType.java

--- src/main/java/uk/gov/digital/ho/hocs/workflow/domain/model/CaseDataType.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/domain/model/CaseDataType.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = {"shortCode"})
 public class CaseDataType {
 
