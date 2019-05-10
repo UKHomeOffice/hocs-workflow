61c2907b07159ea4371cea09e4ac1a88525f3696
Stuart <stuart.whitlam@digital.homeoffice.gov.uk>
2019-05-10 15:51:12 +0100

    fix datetime contract

--- src/main/java/uk/gov/digital/ho/hocs/workflow/client/caseworkclient/dto/GetCaseworkCaseDataResponse.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/client/caseworkclient/dto/GetCaseworkCaseDataResponse.java
@@ -1,11 +1,11 @@
 package uk.gov.digital.ho.hocs.workflow.client.caseworkclient.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.UUID;
 
@@ -17,7 +17,8 @@ public class GetCaseworkCaseDataResponse {
     private UUID uuid;
 
     @JsonProperty("created")
-    private LocalDateTime created;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss Z", timezone = "UTC")
+    private ZonedDateTime created;
 
     @JsonProperty("type")
     private String type;
