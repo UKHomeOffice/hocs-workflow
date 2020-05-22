7d06c41266b466e6da30147e5b9c89bb5f9f5e4c
Parth Vyas <parth.vyas@digital.homeoffice.gov.uk>
2020-05-22 15:37:44 +0100

    HOCS-1739: Fix tests.

--- src/test/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/InfoClientTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/client/infoclient/InfoClientTest.java
@@ -1,17 +1,20 @@
 package uk.gov.digital.ho.hocs.workflow.client.infoclient;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.ParameterizedTypeReference;
+import uk.gov.digital.ho.hocs.workflow.api.dto.SchemaDto;
 import uk.gov.digital.ho.hocs.workflow.application.RestHelper;
+
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InfoClientTest {
@@ -47,10 +50,14 @@ public class InfoClientTest {
         String caseStages = "STAGE1,STAGE2";
         String expectedResourcePath = String.format("/schema/caseType/%s?stages=%s", caseType, caseStages);
 
-        infoClient.getSchemasForCaseTypeAndStages(caseType, caseStages);
+        SchemaDto schemaDto = new SchemaDto();
 
+        when(restHelper.get(eq(infoServiceUrl), eq(expectedResourcePath), any(ParameterizedTypeReference.class)))
+                .thenReturn(List.of(schemaDto));
+        List<SchemaDto> result = infoClient.getSchemasForCaseTypeAndStages(caseType, caseStages);
+
+        Assert.assertEquals(List.of(schemaDto), result);
         verify(restHelper).get(eq(infoServiceUrl), eq(expectedResourcePath), any(ParameterizedTypeReference.class));
-
         verifyNoMoreInteractions(restHelper);
     }
 
