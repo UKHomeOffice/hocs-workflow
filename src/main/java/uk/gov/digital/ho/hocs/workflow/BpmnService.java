0a0b16fb896f1f42d3ef4cb49fd3936d378799d5
Andrew Barker <andrew.barker@digital.homeoffice.gov.uk>
2020-12-03 10:39:35 +0000

    HOCS-2019 : change name of bpmn service method to create a case note

--- src/main/java/uk/gov/digital/ho/hocs/workflow/BpmnService.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/BpmnService.java
@@ -270,10 +270,10 @@ public class BpmnService {
         log.info("Adding Casenote to Case: {}", caseUUIDString);
     }
 
-    public void updateCaseNote(String caseUUIDString, String caseNote, String caseNoteType) {
-        log.debug("######## Save Case Note ########");
+    public void createCaseNote(String caseUUIDString, String caseNote, String caseNoteType) {
+        log.debug("######## Create Case Note ########");
         caseworkClient.createCaseNote(UUID.fromString(caseUUIDString), caseNoteType, caseNote);
-        log.info("Adding Case note to Case: {}", caseUUIDString);
+        log.info("Adding new Case note to Case: {}", caseUUIDString);
     }
 
     public void createCaseConversionNote(String caseUUIDString, String stageUUIDString, String caseConversionNote) {
