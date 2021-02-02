4869c7f5b433126038eb853a71967c96d23bb490
Parth Vyas <parth.vyas@digital.homeoffice.gov.uk>
2021-02-02 15:06:53 +0000

    HOCS-2027: fix/modify integration tests.

--- src/integration-test/java/uk/gov/digital/ho/hocs/workflow/security/SecurityIntegrationTest.java
+++ src/integration-test/java/uk/gov/digital/ho/hocs/workflow/security/SecurityIntegrationTest.java
@@ -18,6 +18,9 @@ import uk.gov.digital.ho.hocs.workflow.api.dto.CreateCaseResponse;
 import uk.gov.digital.ho.hocs.workflow.application.LogEvent;
 import uk.gov.digital.ho.hocs.workflow.application.RequestData;
 import uk.gov.digital.ho.hocs.workflow.client.caseworkclient.CaseworkClient;
+import uk.gov.digital.ho.hocs.workflow.client.caseworkclient.dto.GetCaseworkCaseDataResponse;
+import uk.gov.digital.ho.hocs.workflow.client.caseworkclient.dto.GetCorrespondentResponse;
+import uk.gov.digital.ho.hocs.workflow.client.caseworkclient.dto.GetTopicResponse;
 import uk.gov.digital.ho.hocs.workflow.client.infoclient.InfoClient;
 import uk.gov.digital.ho.hocs.workflow.client.infoclient.dto.PermissionDto;
 import uk.gov.digital.ho.hocs.workflow.client.infoclient.dto.TeamDto;
@@ -25,6 +28,7 @@ import uk.gov.digital.ho.hocs.workflow.domain.exception.ApplicationExceptions;
 import uk.gov.digital.ho.hocs.workflow.api.dto.CaseDataType;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,6 +54,9 @@ public class SecurityIntegrationTest {
     @MockBean
     WorkflowService workflowService;
 
+    @MockBean
+    UserPermissionsService userPermissionsService;
+
     UUID userId = UUID.randomUUID();
     UUID teamUUID = UUID.fromString("44444444-2222-2222-2222-222222222222");
 
@@ -66,9 +73,13 @@ public class SecurityIntegrationTest {
     public void shouldGetStageDataWhenUserAndTeamAreAllocated() {
         UUID caseUUID = UUID.randomUUID();
         UUID stageUUID = UUID.randomUUID();
+
+        setUpAdminTeamLogic(caseUUID, false);
+        when(userPermissionsService.getUserTeams()).thenReturn(Set.of(teamUUID));
         when(caseworkClient.getStageUser(caseUUID, stageUUID)).thenReturn(userId);
         when(caseworkClient.getStageTeam(caseUUID, stageUUID)).thenReturn(teamUUID);
         when(infoClient.getCaseTypeByShortCode(caseUUID.toString().substring(34))).thenReturn(new CaseDataType("MIN", "al"));
+
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
         headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
         HttpEntity httpEntity = new HttpEntity(headers);
@@ -80,9 +91,13 @@ public class SecurityIntegrationTest {
     public void shouldReturnForbiddenWhenUserIsNotAllocated() {
         UUID caseUUID = UUID.randomUUID();
         UUID stageUUID = UUID.randomUUID();
+
+        setUpAdminTeamLogic(caseUUID, false);
+        when(userPermissionsService.getUserTeams()).thenReturn(Set.of(teamUUID));
         when(caseworkClient.getStageTeam(caseUUID, stageUUID)).thenReturn(teamUUID);
         when(caseworkClient.getStageUser(caseUUID, stageUUID)).thenReturn(UUID.randomUUID());
         when(infoClient.getCaseTypeByShortCode(caseUUID.toString().substring(34))).thenReturn(new CaseDataType("MIN", "al"));
+
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
         headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
         HttpEntity httpEntity = new HttpEntity(headers);
@@ -94,6 +109,7 @@ public class SecurityIntegrationTest {
     public void shouldReturnUnauthorisedWhenTeamIsNotAllocated() {
         UUID caseUUID = UUID.randomUUID();
         UUID stageUUID = UUID.randomUUID();
+        setUpAdminTeamLogic(caseUUID, false);
         when(caseworkClient.getStageTeam(caseUUID, stageUUID)).thenReturn(UUID.randomUUID());
         when(infoClient.getCaseTypeByShortCode(caseUUID.toString().substring(34))).thenReturn(new CaseDataType("MIN", "al"));
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
@@ -103,11 +119,11 @@ public class SecurityIntegrationTest {
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
-
     @Test
     public void shouldReturnNotFoundIfCaseUUIDNotFound() {
         UUID caseUUID = UUID.randomUUID();
         UUID stageUUID = UUID.randomUUID();
+        setUpAdminTeamLogic(caseUUID, false);
         when(caseworkClient.getStageTeam(caseUUID, stageUUID)).thenThrow(new ApplicationExceptions.EntityNotFoundException("Stage not found",LogEvent.CASE_NOT_FOUND));
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
         headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
@@ -120,7 +136,13 @@ public class SecurityIntegrationTest {
     public void shouldCreateCaseWhenUserIsInGroup() {
         UUID caseUUID = UUID.randomUUID();
         UUID userUUID = UUID.randomUUID();
-        when(workflowService.createCase("MIN", LocalDate.now(),new ArrayList<>(), userUUID)).thenReturn(new CreateCaseResponse(caseUUID,"CASE_REF"));
+        String caseType = "MIN";
+
+        setUpAdminTeamLogic(caseUUID, false);
+        when(userPermissionsService.getUserTeams()).thenReturn(Set.of(teamUUID));
+        when(userPermissionsService.getMaxAccessLevel(caseType)).thenReturn(AccessLevel.from(5));
+        when(workflowService.createCase(caseType, LocalDate.now(),new ArrayList<>(), userUUID)).thenReturn(new CreateCaseResponse(caseUUID,"CASE_REF"));
+
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
         headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
         HttpEntity<CreateCaseRequest> httpEntity = new HttpEntity<>(new CreateCaseRequest("MIN",LocalDate.now(), new ArrayList<>()), headers);
@@ -130,11 +152,26 @@ public class SecurityIntegrationTest {
 
     @Test
     public void shouldReturnUnauthorisedForCreateCaseWhenUserIsNotInGroup() {
+        String caseType = "TRO";
+        when(userPermissionsService.getMaxAccessLevel(caseType)).thenReturn(AccessLevel.from(0));
         headers.add(RequestData.USER_ID_HEADER, userId.toString());
         headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
-        HttpEntity<CreateCaseRequest> httpEntity = new HttpEntity<>(new CreateCaseRequest("TRO",LocalDate.now(), new ArrayList<>()), headers);
+        HttpEntity<CreateCaseRequest> httpEntity = new HttpEntity<>(new CreateCaseRequest(caseType,LocalDate.now(), new ArrayList<>()), headers);
         ResponseEntity<String> result = restTemplate.exchange( getBasePath()  + "/case", HttpMethod.POST, httpEntity, String.class);
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void shouldCreateCaseWhenUserInAdminTeam() {
+        String caseType = "MIN";
+
+        when(userPermissionsService.getMaxAccessLevel(caseType)).thenReturn(AccessLevel.from(6));
+
+        headers.add(RequestData.USER_ID_HEADER, userId.toString());
+        headers.add(RequestData.GROUP_HEADER, "/RERERCIiIiIiIiIiIiIiIg");
+        HttpEntity<CreateCaseRequest> httpEntity = new HttpEntity<>(new CreateCaseRequest("MIN",LocalDate.now(), new ArrayList<>()), headers);
+        ResponseEntity<String> result = restTemplate.exchange( getBasePath()  + "/case", HttpMethod.POST, httpEntity, String.class);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     private Set<TeamDto>  setupMockTeams(String caseType, int permission) {
@@ -144,6 +181,23 @@ public class SecurityIntegrationTest {
         TeamDto teamDto = new TeamDto("TEAM 1", teamUUID, true, permissions);
         teamDtos.add(teamDto);
         return teamDtos;
+    }
+
+    private void setUpAdminTeamLogic(UUID caseUUID, boolean isAdminTeam) {
+        LocalDate localDate = LocalDate.now();
+
+        String caseType = "SOME_CASE_TYPE";
+        String teamCaseType = isAdminTeam ? "SOME_CASE_TYPE" : "SOME_OTHER_CASE_TYPE";
+        Set<String> adminTeamCaseTypes = Set.of(teamCaseType);
+
+        GetCaseworkCaseDataResponse caseData = new GetCaseworkCaseDataResponse(
+                caseUUID, ZonedDateTime.now(), caseType, "REF", new HashMap<>(), localDate, localDate,
+                UUID.randomUUID(), new GetTopicResponse(), UUID.randomUUID(), new GetCorrespondentResponse()
+        );
+
+        when(caseworkClient.getCase(caseUUID)).thenReturn(caseData);
+        when(userPermissionsService.getCaseTypesIfUserTeamIsCaseTypeAdmin()).thenReturn(adminTeamCaseTypes);
+        when(userPermissionsService.getUserId()).thenReturn(userId);
     }
 
     private String getBasePath() {
