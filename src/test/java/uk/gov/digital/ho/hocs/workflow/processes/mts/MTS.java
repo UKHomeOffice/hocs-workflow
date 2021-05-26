bcbd35561147aa96999332eab02fca6cada04ce4
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-05-26 17:48:16 +0100

    HOCS-2762: camunda tests

---
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/mts/MTS.java
@@ -0,0 +1,73 @@
+package uk.gov.digital.ho.hocs.workflow.processes.mts;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.extension.mockito.ProcessExpressions;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.digital.ho.hocs.workflow.BpmnService;
+import uk.gov.digital.ho.hocs.workflow.util.CallActivityReturnVariable;
+import uk.gov.digital.ho.hocs.workflow.util.ExecutionVariableSequence;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@Deployment(resources = {
+        "processes/MTS.bpmn",
+        "processes/MTS_DATA_INPUT.bpmn",
+        "processes/STAGE.bpmn"
+})
+public class MTS {
+
+    public static final String DATA_INPUT_CALL_ACTIVITY = "CallActivity_14mwb9u";
+
+    public static final String COMPLETE_CASE_SERVICE_TASK = "ServiceTask_1umfuu0";
+
+    public static final String END_EVENT = "EndEvent_0h34pj4";
+
+    @Rule
+    @ClassRule
+    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create()
+            .build();
+
+    @Mock
+    BpmnService bpmnService;
+
+    @Mock
+    private ProcessScenario mtsProcess;
+
+    @Before
+    public void defaultScenario() {
+        Mocks.register("bpmnService", bpmnService);
+
+        ProcessExpressions.registerCallActivityMock("MTS_DATA_INPUT")
+                .deploy(rule);
+    }
+
+    @Test
+    public void happyPath() {
+        Scenario.run(mtsProcess)
+                .startByKey("MTS")
+                .execute();
+
+        verify(mtsProcess)
+                .hasCompleted(DATA_INPUT_CALL_ACTIVITY);
+
+        verify(mtsProcess)
+                .hasCompleted(COMPLETE_CASE_SERVICE_TASK);
+
+        verify(mtsProcess).hasFinished(END_EVENT);
+    }
+}
