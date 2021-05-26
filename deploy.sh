09ff22b3111f85a83e12595bbfab4c2aec12bde0
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-05-26 18:17:54 +0100

    HOCS-2239: add downscaler annotation (#32)
    Presently, when the deployment changes the downscaler annotation 
    (that was manually added up to present) gets removed. This causes 
    pod to stay up indefinitely, which leads to increased costs and 
    potential issues. This change enforced the uptime meta-data 
    annotation that enforces this server stays up. This is specified 
    differently depending on if we are in prod or notprod.

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,13 @@ if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
     export MIN_REPLICAS="2"
     export MAX_REPLICAS="6"
+
+    export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="2"
+
+    export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
 fi
 
 cd kd
