17ffdd4084a44d32d53efc9e7c7f44d90c3f134f
Sam Gardner <sgardner@caci.co.uk>
2021-06-02 16:48:19 +0100

    HOCS-2785 Adding tests for COMP_SERVICE_TRIAGE

---
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/COMP_SERVICE_TRIAGE.java
@@ -0,0 +1,143 @@
+package uk.gov.digital.ho.hocs.workflow.processes;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.digital.ho.hocs.workflow.BpmnService;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@Deployment(resources = "processes/COMP_SERVICE_TRIAGE.bpmn")
+public class COMP_SERVICE_TRIAGE {
+
+    @Rule
+    @ClassRule
+    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().assertClassCoverageAtLeast(1).build();
+
+    @Rule
+    public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+
+    @Mock
+    private BpmnService bpmnService;
+
+    @Mock
+    private ProcessScenario compServiceTriageProcess;
+
+    @Before
+    public void setup() {
+        Mocks.register("bpmnService", bpmnService);
+    }
+
+    @Test
+    public void testRejectTriage(){
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Accept"))
+                .thenReturn(task -> task.complete(withVariables("valid", false)))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "CctTriageAccept", "No")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Transfer"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        Scenario.run(compServiceTriageProcess).startByKey("COMP_SERVICE_TRIAGE").execute();
+
+        verify(compServiceTriageProcess, times(3)).hasCompleted("Screen_Accept");
+        verify(compServiceTriageProcess, times(3)).hasCompleted("Screen_Transfer");
+        verify(compServiceTriageProcess).hasCompleted("Service_UpdateAllocationNote");
+    }
+
+    @Test
+    public void testTriageResultDraft(){
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Accept"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "CctTriageAccept", "Yes")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Details"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_BusArea"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Input"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CctTriageResult", "Pending")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CctTriageResult", "NotPending", "CctTriageResult", "Draft")));
+
+        Scenario.run(compServiceTriageProcess).startByKey("COMP_SERVICE_TRIAGE").execute();
+
+        verify(compServiceTriageProcess).hasCompleted("Service_UpdateTeamByStageAndTexts_Draft");
+    }
+
+    @Test
+    public void testTriageResultEscalate(){
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Accept"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "CctTriageAccept", "Yes")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Details"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_BusArea"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Input"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CctTriageResult", "NotPending", "CctTriageResult", "Escalate")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Escalate"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        Scenario.run(compServiceTriageProcess).startByKey("COMP_SERVICE_TRIAGE").execute();
+
+        verify(compServiceTriageProcess).hasCompleted("Service_UpdateAllocationNote_Escalate");
+        verify(compServiceTriageProcess).hasCompleted("Service_UpdateTeamByStageAndTexts_Escalate");
+    }
+
+    @Test
+    public void testTriageResultComplete(){
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Accept"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "CctTriageAccept", "Yes")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Details"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_BusArea"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_Input"))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CctTriageResult", "NotPending", "CctTriageResult", "Complete")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_CompleteReason"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD")));
+
+        when(compServiceTriageProcess.waitsAtUserTask("Validate_CompleteConfirm"))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", false, "DIRECTION", "FORWARD")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CompleteResult", "No")))
+                .thenReturn(task -> task.complete(withVariables("valid", true, "DIRECTION", "FORWARD", "CompleteResult", "Yes")));
+
+        Scenario.run(compServiceTriageProcess).startByKey("COMP_SERVICE_TRIAGE").execute();
+
+        verify(compServiceTriageProcess).hasCompleted("Service_UpdateAllocationNote_Complete");
+    }
+
+}
