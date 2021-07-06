dc416b6206fcab3ba6444949c394f67fdda3f6bf
Gary Newbolt <gary.newbolt@digital.homeoffice.gov.uk>
2021-07-06 09:03:04 +0100

    HOCS-3286: update workflow with passthrough service

--- src/main/java/uk/gov/digital/ho/hocs/workflow/domain/model/forms/HocsFormField.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/domain/model/forms/HocsFormField.java
@@ -3,6 +3,7 @@ package uk.gov.digital.ho.hocs.workflow.domain.model.forms;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import uk.gov.digital.ho.hocs.workflow.api.dto.FieldDto;
 import uk.gov.digital.ho.hocs.workflow.client.infoclient.dto.CaseDetailsFieldDto;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor()
 @Getter
+@ToString
 public class HocsFormField {
 
     protected String component;
