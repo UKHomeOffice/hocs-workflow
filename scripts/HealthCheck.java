d32c2d4b60fe8cd84078c14c9bb8422b632deaa8
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2021-07-06 12:52:44 +0100

    HOCS-3087 Making health check file more generic

--- scripts/HealthCheck.java
+++ scripts/HealthCheck.java
@@ -8,20 +8,26 @@ import java.net.http.HttpResponse;
 
 /**
  * Class used for container health check by docker compose
+ * Pass address to be health checked as the single command line argument
  */
 public class HealthCheck {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        if(args.length < 1){
+            throw new IllegalArgumentException("No URL received as command line argument");
+        }
+
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/actuator/health"))
+                .uri(URI.create(args[0]))
                 .header("accept", "application/json")
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() != 200 || !response.body().contains("UP")) {
-            throw new RuntimeException("Healthcheck failed with status code: " + response.statusCode());
+            throw new RuntimeException("Health check to " + args[0] + " failed with status code: " + response.statusCode());
         }
     }
 
