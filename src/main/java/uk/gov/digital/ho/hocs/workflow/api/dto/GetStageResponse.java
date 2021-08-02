6e69d27cee2ac927808ce2856dac0e3b9c7d0acd
Gary Newbolt <32836753+gary-newbolt@users.noreply.github.com>
2021-08-02 16:42:24 +0100

    HOCS-3286: remove foi data input stage (#474)
    * HOCS-3286: new create case spike initial
    
    * HOCS-3286: update workflow with passthrough service
    
    * HOCS-3286: production code clean up
    
    * HOCS-3286: remove erroneous file change
    
    * HOCS-3286: remove foi data input stage
    
    * HOCS-3386: removal of workflow and refactor tests
    
    * HOCS-3386: merge conflict fixes

--- src/main/java/uk/gov/digital/ho/hocs/workflow/api/dto/GetStageResponse.java
+++ src/main/java/uk/gov/digital/ho/hocs/workflow/api/dto/GetStageResponse.java
@@ -3,7 +3,6 @@ package uk.gov.digital.ho.hocs.workflow.api.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 import uk.gov.digital.ho.hocs.workflow.domain.model.forms.HocsForm;
 
 import java.util.UUID;
