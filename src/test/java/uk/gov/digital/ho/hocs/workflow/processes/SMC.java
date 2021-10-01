8736b75c141e1c485e25eae1c49e41badc332f79
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2021-10-01 12:20:47 +0100

    HOCS-3791 Add triage stage to SMC

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/SMC.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/SMC.java
@@ -22,12 +22,14 @@ import static uk.gov.digital.ho.hocs.workflow.util.CallActivityMockWrapper.whenA
 @RunWith(MockitoJUnitRunner.class)
 @Deployment(resources = {"processes/SMC.bpmn",
         "processes/SMC_REGISTRATION.bpmn",
+        "processes/SMC_TRIAGE.bpmn",
+        "processes/SMC_SEND.bpmn",
         "processes/STAGE.bpmn"})
 public class SMC {
 
     @Rule
     @ClassRule
-    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().assertClassCoverageAtLeast(0.3).build();
+    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().assertClassCoverageAtLeast(0.1).build();
 
     @Rule
     public ProcessEngineRule processEngineRule = new ProcessEngineRule();
@@ -49,12 +51,22 @@ public class SMC {
                 .thenReturn("valid", "true")
                 .deploy(rule);
 
+        whenAtCallActivity("SMC_TRIAGE")
+                .thenReturn("valid", "true")
+                .deploy(rule);
+
+        whenAtCallActivity("SMC_SEND")
+                .thenReturn("valid", "true")
+                .deploy(rule);
+
         Scenario.run(processScenario)
                 .startByKey("SMC")
                 .execute();
 
         verify(processScenario).hasCompleted("StartEvent_SMC");
         verify(processScenario).hasCompleted("CallActivity_SMC_REGISTRATION");
+        verify(processScenario).hasCompleted("CallActivity_SMC_TRIAGE");
+        verify(processScenario).hasCompleted("CallActivity_SMC_SEND");
         verify(processScenario).hasCompleted("ServiceTask_CompleteCase");
         verify(processScenario).hasCompleted("EndEvent_SMC");
     }
