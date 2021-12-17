12903b6f3279c98400d5170dee8768afc231003b
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-12-17 00:43:08 +0000

    DEPENDABOT add dependabot to hocs-workflow project

--- src/test/java/uk/gov/digital/ho/hocs/workflow/util/CallActivityVariable.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/util/CallActivityVariable.java
@@ -1,11 +1,15 @@
 package uk.gov.digital.ho.hocs.workflow.util;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor()
+@EqualsAndHashCode
+@ToString
 public class CallActivityVariable {
     private final String source;
     private final String target;
-
-    public CallActivityVariable(String source, String target) {
-        this.source = source;
-        this.target = target;
-    }
 }
