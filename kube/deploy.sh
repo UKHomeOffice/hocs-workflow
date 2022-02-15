6d4d9e54e1a643d74b90ef707a45bfea2e17100e
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2022-02-15 23:35:17 +0000

    HOCS-4580 Specify TLS cert for kd

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -10,18 +10,22 @@ if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
     export MIN_REPLICAS="2"
     export MAX_REPLICAS="3"
+    export CLUSTER_NAME="acp-prod"
 
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
 else
     export MIN_REPLICAS="1"
     export MAX_REPLICAS="2"
+    export CLUSTER_NAME="acp-notprod"
 
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
 fi
 
+export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
+
 cd kd
 
-kd --insecure-skip-tls-verify \
+kd \
    --timeout 10m \
     -f deployment.yaml \
     -f service.yaml \
