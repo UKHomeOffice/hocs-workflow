70d8d9623336cdbf600c43852118244e90184cfb
abarker66 <71025533+abarker66@users.noreply.github.com>
2022-07-19 10:04:34 +0100

    Feature/hocs 4648 pogr dispatch stage (#739)
    * HOCS-4643: Add QA stages
    
    Add the POGR QA BPMN files. To allow for the user to be kept between all
     Drafting stages the individual stages now pass around the associated
     user and utilise STAGE_WITH_USER rather than STAGE.
    
    * HOCS-4648 add pogr dispatch stage
    
    * HOCS-4648 update bpmn diagrams
    
    * HOCS-4648 add dispatch stage tests
    
    * HOCS-4648 add dispatch stage updates
    
    * HOCS-4648 merge with QA
    
    * HOCS-4648 update dispatch tests
    
    Co-authored-by: Joseph Rogerson <joseph.rogerson@digital.homeoffice.gov.uk>

---
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr/POGR_HMPO_DISPATCH.java
@@ -0,0 +1,60 @@
+package uk.gov.digital.ho.hocs.workflow.processes.pogr;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.digital.ho.hocs.workflow.BpmnService;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@Deployment(resources = {
+        "processes/POGR/POGR_HMPO_DISPATCH.bpmn"
+})
+public class POGR_HMPO_DISPATCH {
+
+    @Rule
+    @ClassRule
+    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().assertClassCoverageAtLeast(1).build();
+
+    @Rule
+    public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+
+    @Mock
+    BpmnService bpmnService;
+
+    @Mock
+    private ProcessScenario processScenario;
+
+    @Before
+    public void setup() {
+        Mocks.register("bpmnService", bpmnService);
+    }
+
+    @Test
+    public void testHappyPath() {
+        when(processScenario.waitsAtUserTask("Screen_Dispatch_Input"))
+                .thenReturn(task -> task.complete(withVariables("DispatchOutcome", "Upheld")));
+
+        Scenario.run(processScenario)
+                .startByKey("POGR_HMPO_DISPATCH")
+                .execute();
+
+        verify(processScenario).hasCompleted("EndEvent_HmpoQa");
+    }
+
+}
