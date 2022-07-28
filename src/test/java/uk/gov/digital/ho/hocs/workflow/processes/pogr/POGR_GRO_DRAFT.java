0a247dab30c7d44c18ddb9ab0e04d24c58b0f311
Vinoth Srinivasan <vinothsrinivasan@hod083459.local>
2022-07-28 15:47:41 +0100

    HOCS-4943: add draft case closure
    Add early case closure at Draft stage. Upon completion of the
    `UserActivity_CloseCase` activity, if the direction is `FORWARD` then
    the `CloseCaseDraft` output is set to true which is used in the main
    flow to indicate an early escape and closure.

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr/POGR_GRO_DRAFT.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr/POGR_GRO_DRAFT.java
@@ -165,4 +165,28 @@ public class POGR_GRO_DRAFT {
         verify(processScenario).hasCompleted("EndEvent_GroDraft");
     }
 
+    @Test
+    public void testCloseCaseAtDraftStage() {
+        whenAtCallActivity("POGR_GRO_PRIORITY_CHANGE_SCREEN")
+                .thenReturn("DraftOutcome", "CloseCase")
+                .thenReturn("DraftOutcome", "CloseCase")
+                .deploy(rule);
+
+        when(processScenario.waitsAtUserTask("UserActivity_CloseCase"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","FORWARD")));
+
+        Scenario.run(processScenario)
+                .startByKey("POGR_GRO_DRAFT")
+                .execute();
+
+        verify(processScenario).hasCompleted("StartEvent_GroDraft");
+        verify(processScenario, times(2)).hasCompleted("CallActivity_DraftInput");
+        verify(processScenario, times(3)).hasCompleted("UserActivity_CloseCase");
+        verify(processScenario).hasCompleted("ServiceTask_SaveCloseNote");
+        verify(bpmnService).updateAllocationNote(any(), any(), any(), eq("Closed"));
+        verify(processScenario).hasCompleted("EndEvent_GroDraft");
+    }
+
 }
