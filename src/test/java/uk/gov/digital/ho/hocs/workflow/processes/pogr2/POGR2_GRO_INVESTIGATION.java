77b1cdad7617730c404e73d2866037b334db80c4
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-08-23 15:02:12 +0100

    HOCS-5035: add POGR Stage 2 Investigation
    Add the POGR2 Stage 2 implementation of the Investigation stage. This
    effectively mimics the stage 1 version.

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr2/POGR2_GRO_INVESTIGATION.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr2/POGR2_GRO_INVESTIGATION.java
@@ -3,10 +3,10 @@ package uk.gov.digital.ho.hocs.workflow.processes.pogr2;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.mock.Mocks;
-import org.camunda.bpm.scenario.ProcessScenario;
-import org.camunda.bpm.scenario.Scenario;
 import org.camunda.community.process_test_coverage.junit4.platform7.rules.TestCoverageProcessEngineRule;
 import org.camunda.community.process_test_coverage.junit4.platform7.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -52,15 +52,24 @@ public class POGR2_GRO_INVESTIGATION {
     public void testHappyPath() {
         when(processScenario.waitsAtUserTask("Screen_InvestigationAcceptCase"))
                 .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "")))
+                .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "Accept")))
                 .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "Accept")));
+
+        whenAtCallActivity("POGR_GRO_PRIORITY_CHANGE_SCREEN")
+                .thenReturn("DIRECTION", "BACKWARD")
+                .thenReturn("DIRECTION", "")
+                .thenReturn("DIRECTION", "FORWARD","InvestigationOutcome", "Pending")
+                .thenReturn("DIRECTION", "FORWARD","InvestigationOutcome", "Draft")
+                .deploy(rule);
 
         Scenario.run(processScenario)
                 .startByKey("POGR2_GRO_INVESTIGATION")
                 .execute();
 
-        verify(processScenario).hasCompleted("StartEvent_POGR_GRO_INVESTIGATION");
-        verify(processScenario, times(2)).hasCompleted("Screen_InvestigationAcceptCase");
-        verify(processScenario).hasCompleted("EndEvent_POGR_GRO_INVESTIGATION");
+        verify(processScenario).hasCompleted("StartEvent_POGR2_GRO_INVESTIGATION");
+        verify(processScenario, times(3)).hasCompleted("Screen_InvestigationAcceptCase");
+        verify(processScenario, times(4)).hasCompleted("CallActivity_Investigation");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_GRO_INVESTIGATION");
     }
 
     @Test
@@ -80,14 +89,14 @@ public class POGR2_GRO_INVESTIGATION {
                 .startByKey("POGR2_GRO_INVESTIGATION")
                 .execute();
 
-        verify(processScenario).hasCompleted("StartEvent_POGR_GRO_INVESTIGATION");
+        verify(processScenario).hasCompleted("StartEvent_POGR2_GRO_INVESTIGATION");
         verify(processScenario, times(2)).hasCompleted("Screen_InvestigationAcceptCase");
         verify(processScenario, times(3)).hasCompleted("Screen_GroTransferScreen");
         verify(processScenario).hasCompleted("Activity_RejectCaseNote");
-        verify(processScenario).hasCompleted("EndEvent_POGR_GRO_INVESTIGATION");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_GRO_INVESTIGATION");
 
         verify(bpmnService).updateAllocationNote(any(), any(), eq("Test"), eq("REJECT"));
-        verify(bpmnService, times(0)).updateTeamByStageAndTexts(any(), any(), eq("POGR_GRO_INVESTIGATION"), any(), any(), any());
+        verify(bpmnService, times(0)).updateTeamByStageAndTexts(any(), any(), eq("POGR2_GRO_INVESTIGATION"), any(), any(), any());
     }
 
     @Test
@@ -100,20 +109,74 @@ public class POGR2_GRO_INVESTIGATION {
                 .thenReturn(task -> task.complete(
                         withVariables("DIRECTION", "FORWARD",
                                 "InvestigationRejectOptions", "Internal",
-                                                    "CaseNote_InvestigationReject", "Test")));
+                                "CaseNote_InvestigationReject", "Test")));
 
         Scenario.run(processScenario)
                 .startByKey("POGR2_GRO_INVESTIGATION")
                 .execute();
 
-        verify(processScenario).hasCompleted("StartEvent_POGR_GRO_INVESTIGATION");
+        verify(processScenario).hasCompleted("StartEvent_POGR2_GRO_INVESTIGATION");
         verify(processScenario, times(2)).hasCompleted("Screen_InvestigationAcceptCase");
         verify(processScenario, times(2)).hasCompleted("Screen_GroTransferScreen");
         verify(processScenario).hasCompleted("Activity_RejectCaseNote");
-        verify(processScenario).hasCompleted("EndEvent_POGR_GRO_INVESTIGATION");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_GRO_INVESTIGATION");
 
         verify(bpmnService).updateAllocationNote(any(), any(), eq("Test"), eq("REJECT"));
         verify(bpmnService).updateTeamByStageAndTexts(any(), any(), eq("POGR2_GRO_INVESTIGATION"), eq("DraftTeamUUID"), eq("DraftTeamName"), eq("InvestigationRejectGroInvestigatingTeamSelection"));
     }
 
+    @Test
+    public void testInvestigationEscalate() {
+        when(processScenario.waitsAtUserTask("Screen_InvestigationAcceptCase"))
+                .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "Accept")));
+
+        whenAtCallActivity("POGR_GRO_PRIORITY_CHANGE_SCREEN")
+                .thenReturn("DIRECTION", "FORWARD", "InvestigationOutcome", "Escalate")
+                .thenReturn("DIRECTION", "FORWARD", "InvestigationOutcome", "Escalate")
+                .deploy(rule);
+
+        when(processScenario.waitsAtUserTask("Screen_GroEscalateScreen"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables(
+                        "DIRECTION","FORWARD",
+                        "CaseNote_InvestigationEscalate", "Test")));
+
+        Scenario.run(processScenario)
+                .startByKey("POGR2_GRO_INVESTIGATION")
+                .execute();
+
+        verify(processScenario).hasCompleted("StartEvent_POGR2_GRO_INVESTIGATION");
+        verify(processScenario, times(2)).hasCompleted("CallActivity_Investigation");
+        verify(processScenario, times(3)).hasCompleted("Screen_GroEscalateScreen");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_GRO_INVESTIGATION");
+
+        verify(bpmnService).updateAllocationNote(any(), any(), eq("Test"), eq("SEND_TO_WORKFLOW_MANAGER"));
+
+    }
+
+    @Test
+    public void testCloseCase() {
+        when(processScenario.waitsAtUserTask("Screen_InvestigationAcceptCase"))
+                .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "Accept")));
+
+        whenAtCallActivity("POGR_GRO_PRIORITY_CHANGE_SCREEN")
+                .thenReturn("DIRECTION", "FORWARD", "InvestigationOutcome", "Complete")
+                .thenReturn("DIRECTION", "FORWARD", "InvestigationOutcome", "Complete")
+                .deploy(rule);
+
+        whenAtCallActivity("POGR_CLOSE_CASE")
+                .thenReturn("CloseCase", Boolean.toString(false))
+                .thenReturn("CloseCase", Boolean.toString(true))
+                .deploy(rule);
+
+        Scenario.run(processScenario)
+                .startByKey("POGR2_GRO_INVESTIGATION")
+                .execute();
+
+        verify(processScenario).hasCompleted("StartEvent_POGR2_GRO_INVESTIGATION");
+        verify(processScenario, times(2)).hasCompleted("CallActivity_Investigation");
+        verify(processScenario, times(2)).hasCompleted("CallActivity_InvestigationCloseCase");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_GRO_INVESTIGATION");
+    }
 }
--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr2/POGR2_HMPO_INVESTIGATION.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/pogr2/POGR2_HMPO_INVESTIGATION.java
@@ -3,10 +3,10 @@ package uk.gov.digital.ho.hocs.workflow.processes.pogr2;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.mock.Mocks;
-import org.camunda.bpm.scenario.ProcessScenario;
-import org.camunda.bpm.scenario.Scenario;
 import org.camunda.community.process_test_coverage.junit4.platform7.rules.TestCoverageProcessEngineRule;
 import org.camunda.community.process_test_coverage.junit4.platform7.rules.TestCoverageProcessEngineRuleBuilder;
+import org.camunda.bpm.scenario.ProcessScenario;
+import org.camunda.bpm.scenario.Scenario;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -17,9 +17,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.digital.ho.hocs.workflow.BpmnService;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.digital.ho.hocs.workflow.util.CallActivityMockWrapper.whenAtCallActivity;
 
 @RunWith(MockitoJUnitRunner.class)
 @Deployment(resources = {
@@ -51,13 +54,20 @@ public class POGR2_HMPO_INVESTIGATION {
                 .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "")))
                 .thenReturn(task -> task.complete(withVariables("InvestigationAccept", "Accept")));
 
+        when(processScenario.waitsAtUserTask("Screen_Investigation"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "InvestigationOutcome", "Pending")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "InvestigationOutcome", "Draft")));
+
         Scenario.run(processScenario)
                 .startByKey("POGR2_HMPO_INVESTIGATION")
                 .execute();
 
-        verify(processScenario).hasCompleted("StartEvent_POGR_HMPO_INVESTIGATION");
-        verify(processScenario, times(2)).hasCompleted("Screen_InvestigationAcceptCase");
-        verify(processScenario).hasCompleted("EndEvent_POGR_HMPO_INVESTIGATION");
+        verify(processScenario).hasCompleted("StartEvent_POGR2_HMPO_INVESTIGATION");
+        verify(processScenario, times(3)).hasCompleted("Screen_InvestigationAcceptCase");
+        verify(processScenario, times(4)).hasCompleted("Screen_Investigation");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_HMPO_INVESTIGATION");
     }
 
     @Test
@@ -69,10 +79,59 @@ public class POGR2_HMPO_INVESTIGATION {
                 .startByKey("POGR2_HMPO_INVESTIGATION")
                 .execute();
 
-        verify(processScenario).hasCompleted("StartEvent_POGR_HMPO_INVESTIGATION");
+        verify(processScenario).hasCompleted("StartEvent_POGR2_HMPO_INVESTIGATION");
         verify(processScenario).hasCompleted("Screen_InvestigationAcceptCase");
         verify(processScenario).hasCompleted("Activity_RejectCaseNote");
-        verify(processScenario).hasCompleted("EndEvent_POGR_HMPO_INVESTIGATION");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_HMPO_INVESTIGATION");
+    }
+
+    @Test
+    public void testInvestigationEscalate() {
+        when(processScenario.waitsAtUserTask("Screen_InvestigationAcceptCase"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "InvestigationAccept", "Accept")));
+
+        when(processScenario.waitsAtUserTask("Screen_Investigation"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD","InvestigationOutcome", "Escalate")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD","InvestigationOutcome", "Escalate")));
+
+        when(processScenario.waitsAtUserTask("Screen_HmpoEscalateScreen"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","BACKWARD")))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION","FORWARD", "CaseNote_InvestigationEscalate", "Test")));
+
+        Scenario.run(processScenario)
+                .startByKey("POGR2_HMPO_INVESTIGATION")
+                .execute();
+
+        verify(processScenario).hasCompleted("StartEvent_POGR2_HMPO_INVESTIGATION");
+        verify(processScenario, times(2)).hasCompleted("Screen_Investigation");
+        verify(processScenario, times(3)).hasCompleted("Screen_HmpoEscalateScreen");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_HMPO_INVESTIGATION");
+
+        verify(bpmnService).updateAllocationNote(any(), any(), eq("Test"), eq("SEND_TO_WORKFLOW_MANAGER"));
+    }
+
+    @Test
+    public void testCloseCase() {
+        when(processScenario.waitsAtUserTask("Screen_InvestigationAcceptCase"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "InvestigationAccept", "Accept")));
+
+        when(processScenario.waitsAtUserTask("Screen_Investigation"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD","InvestigationOutcome", "Complete")));
+
+        whenAtCallActivity("POGR_CLOSE_CASE")
+                .thenReturn("CloseCase", Boolean.toString(false))
+                .thenReturn("CloseCase", Boolean.toString(true))
+                .deploy(rule);
+
+        Scenario.run(processScenario)
+                .startByKey("POGR2_HMPO_INVESTIGATION")
+                .execute();
+
+        verify(processScenario).hasCompleted("StartEvent_POGR2_HMPO_INVESTIGATION");
+        verify(processScenario, times(2)).hasCompleted("Screen_Investigation");
+        verify(processScenario, times(2)).hasCompleted("CallActivity_InvestigationCloseCase");
+        verify(processScenario).hasCompleted("EndEvent_POGR2_HMPO_INVESTIGATION");
     }
 
 }
