8a783fce0c040a4d198e247a9bec6c06ed03ce78
Alex Whyte <alexander.whyte@digital.homeoffice.gov.uk>
2022-09-12 10:58:13 +0100

    updated task names and added test scenario

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/iedet/IEDET_REGISTRATION.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/iedet/IEDET_REGISTRATION.java
@@ -156,4 +156,20 @@ public class IEDET_REGISTRATION {
         verify(compRegistrationProcess)
                 .hasCompleted("Service_CaseHasPrimaryCorrespondentType");
     }
+
+    @Test
+    public void hasComplaintTypeOfSeriousMisconduct() {
+        when(compRegistrationProcess.waitsAtUserTask("Validate_Complaint"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD","CompType", "SeriousMisconduct")));
+
+        when(compRegistrationProcess.waitsAtUserTask("Activity_0bf3jlz"))
+                .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD")));
+
+        Scenario.run(compRegistrationProcess)
+                .startByKey("IEDET_REGISTRATION")
+                .execute();
+
+        verify(compRegistrationProcess)
+                .hasCompleted("Service_CaseHasPrimaryCorrespondentType");
+    }
 }
