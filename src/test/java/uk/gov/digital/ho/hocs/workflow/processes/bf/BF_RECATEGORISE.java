0c1cd81bdf0bda293fb81caa52f54763ce92c759
Alex Whyte <alexander.whyte@digital.homeoffice.gov.uk>
2023-01-30 13:51:50 +0000

    HOCS-5916: BF PSU complaint categories for recategorise

--- src/test/java/uk/gov/digital/ho/hocs/workflow/processes/bf/BF_RECATEGORISE.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/processes/bf/BF_RECATEGORISE.java
@@ -70,8 +70,14 @@ public class BF_RECATEGORISE {
         when(bfRecategoriseProcess.waitsAtUserTask("Activity_ScreenComplaintType"))
             .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "CompType", "SeriousMisconduct")));
 
+        when(bfRecategoriseProcess.waitsAtUserTask("Activity_ScreenCategorySerious"))
+            .thenReturn(task -> task.complete(withVariables("DIRECTION", "")))
+            .thenReturn(task -> task.complete(withVariables("DIRECTION", "BACKWARD")))
+            .thenReturn(task -> task.complete(withVariables("DIRECTION", "FORWARD", "OtherUnprof", "true")));
+
         Scenario.run(bfRecategoriseProcess).startByKey("BF_RECATEGORISE").execute();
 
-        verify(bfRecategoriseProcess, times(1)).hasCompleted("Activity_ScreenComplaintType");
+        verify(bfRecategoriseProcess, times(2)).hasCompleted("Activity_ScreenComplaintType");
+        verify(bfRecategoriseProcess, times(3)).hasCompleted("Activity_ScreenCategorySerious");
     }
 }
