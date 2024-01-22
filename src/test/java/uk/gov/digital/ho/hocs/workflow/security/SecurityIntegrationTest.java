b2ee4e4b649efc0a8774a0d696fdb914df38c188
vinothon84 <vinothon84@gmail.com>
2024-01-22 11:57:21 +0000

    HOCS-6854:Build changes for spring boot 3.1.5 upgrade

--- src/test/java/uk/gov/digital/ho/hocs/workflow/security/SecurityIntegrationTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/workflow/security/SecurityIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
